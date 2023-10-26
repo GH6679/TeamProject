25441053c5d9781aa48267d828e5e09795e5d51a
YoonGwngHyuck <YGH@YGH-Laptop>
2023-10-26 11:26:35 +0900

    10-26-5

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -16,7 +16,7 @@ public class DataSourceConfig {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/bookdb");
         dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+        dataSource.setPassword("12345678");
 
         return dataSource;
     }
